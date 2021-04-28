45e3007517e909f956b2d6d3b603096c68e5a036
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-04-28 17:54:37 +0200

    ADD new photo file to cloudinary

--- configs/cloudinary.config.js
+++ configs/cloudinary.config.js
@@ -11,7 +11,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        allowed_formats: ['jpg'],
+        allowed_formats: ['jpg', 'png'],
         folder: 'wallapoop-project-2'
     }
 })
